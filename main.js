4044a667195c45fc7464d818a834f52a679cf867
ana0 <sarah.anne.friend@gmail.com>
2025-04-25 19:55:49 +0700

    Add mastodon

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { AtpAgent } from '@atproto/api';
+import { createRestAPIClient } from "masto";
 import * as dotenv from 'dotenv';
 import * as process from 'process';
 
@@ -9,12 +10,21 @@ const agent = new AtpAgent({
     service: 'https://bsky.social',
   })
 
+  const masto = createRestAPIClient({
+    url: process.env.MASTODON_URL,
+    accessToken: process.env.MASTODON_ACCESSTOKEN,
+  });
+
 
 async function main() {
+    const myPost = "This is a test, I'm posting from a script"
     await agent.login({ identifier: process.env.BLUESKY_USERNAME, password: process.env.BLUESKY_PASSWORD})
     await agent.post({
-        text: "This is a test, I'm posting from a script",
+        text: myPost,
     });
+    await masto.v1.statuses.create({
+        status: myPost,
+      });
     console.log("Just posted!")
 }
 
